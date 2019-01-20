7540f34e32ff58e08ecd130c01c2023fc321a5d6
WolfyScript <wolfyscript@gmail.com>
2019-01-20 14:30:35 +0100

    Added language config and main config

--- src/me/wolfyscript/customcrafting/CustomCrafting.java
+++ src/me/wolfyscript/customcrafting/CustomCrafting.java
@@ -1,14 +1,21 @@
 package me.wolfyscript.customcrafting;
 
+import me.wolfyscript.utilities.api.WolfyUtilities;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomCrafting extends JavaPlugin {
 
     private static Plugin instance;
+    private static WolfyUtilities api;
 
     public void onEnable() {
         instance = this;
+        api = new WolfyUtilities(instance);
+        api.setCHAT_PREFIX("§7[§6CC§7] ");
+        api.setCONSOLE_PREFIX("§7[§3CC§7] ");
+
+
 
     }
 
@@ -20,5 +27,9 @@ public class CustomCrafting extends JavaPlugin {
 
     public static Plugin getInst() {
         return instance;
+    }
+
+    public static WolfyUtilities getApi() {
+        return api;
     }
 }
