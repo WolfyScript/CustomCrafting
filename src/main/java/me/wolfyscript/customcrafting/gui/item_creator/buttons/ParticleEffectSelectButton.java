fb3159fec71150430d796f3a88c62c16b7c81d23
BuildTools <wolfyscript@gmail.com>
2020-01-21 18:52:45 +0100

    Updated pom.xml

--- src/main/java/me/wolfyscript/customcrafting/gui/item_creator/buttons/ParticleEffectSelectButton.java
+++ src/main/java/me/wolfyscript/customcrafting/gui/item_creator/buttons/ParticleEffectSelectButton.java
@@ -2,11 +2,13 @@ package me.wolfyscript.customcrafting.gui.item_creator.buttons;
 
 import me.wolfyscript.customcrafting.data.TestCache;
 import me.wolfyscript.customcrafting.data.cache.items.Items;
+import me.wolfyscript.utilities.api.custom_items.ParticleData;
 import me.wolfyscript.utilities.api.inventory.GuiHandler;
 import me.wolfyscript.utilities.api.inventory.button.ButtonActionRender;
 import me.wolfyscript.utilities.api.inventory.button.ButtonState;
 import me.wolfyscript.utilities.api.inventory.button.buttons.ActionButton;
 import me.wolfyscript.utilities.api.utils.particles.ParticleEffect;
+import me.wolfyscript.utilities.api.utils.particles.ParticleEffects;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -26,19 +28,17 @@ public class ParticleEffectSelectButton extends ActionButton {
             public boolean run(GuiHandler guiHandler, Player player, Inventory inventory, int i, InventoryClickEvent inventoryClickEvent) {
 
                 guiHandler.openCluster("particle_creator");
-
                 return false;
             }
 
             @Override
             public ItemStack render(HashMap<String, Object> hashMap, GuiHandler guiHandler, Player player, ItemStack itemStack, int i, boolean b) {
                 Items items = ((TestCache) guiHandler.getCustomCache()).getItems();
-
-                items.getItem().getCustomData("particle");
-                //Should I put the Particle Data directly into the CustomItem?
-                //I tend to da that, because it's better for compatibility when I will ad functionality to it later on!
-
-
+                ParticleData particleData = items.getItem().getParticleData();
+                ParticleEffect particleEffect = ParticleEffects.getEffect(particleData.getParticleEffect(action));
+                if (particleEffect != null) {
+                    itemStack = particleEffect.getIconItem();
+                }
                 return itemStack;
             }
         }));
