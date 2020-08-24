32e864b24f0024f783d3e952df3d3dfbfca24f1d
WolfyScript <wolfyscript@gmail.com>
2020-08-24 17:51:57 +0200

    Advanced Workbench bug fixes Removed old Advanced Workbench placement detection Fixed Advanced Workbench not registering when placed!

--- src/main/java/me/wolfyscript/customcrafting/listeners/BlockListener.java
+++ src/main/java/me/wolfyscript/customcrafting/listeners/BlockListener.java
@@ -4,17 +4,12 @@ import me.wolfyscript.customcrafting.CustomCrafting;
 import me.wolfyscript.utilities.api.WolfyUtilities;
 import me.wolfyscript.utilities.api.custom_items.CustomItemBreakEvent;
 import me.wolfyscript.utilities.api.custom_items.CustomItemPlaceEvent;
-import me.wolfyscript.utilities.api.custom_items.CustomItems;
-import me.wolfyscript.utilities.api.custom_items.api_references.WolfyUtilitiesRef;
 import me.wolfyscript.utilities.api.utils.NamespacedKey;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class BlockListener implements Listener {
 
@@ -24,35 +19,10 @@ public class BlockListener implements Listener {
         this.api = api;
     }
 
-    /*
-    Old Advanced Workbench registering System made compatible with new customItem placing system.
-     */
     @EventHandler(priority = EventPriority.HIGH)
     public void onBlockPlace(CustomItemPlaceEvent event) {
-        if (event.getCustomItem().getApiReference() instanceof WolfyUtilitiesRef && ((WolfyUtilitiesRef) event.getCustomItem().getApiReference()).getNamespacedKey().equals(NamespacedKey.getByString("customcrafting:workbench"))) {
+        if (event.getCustomItem().hasNamespacedKey() && event.getCustomItem().getNamespacedKey().equals(NamespacedKey.getByString("customcrafting:workbench"))) {
             CustomCrafting.getWorkbenches().addWorkbench(event.getBlockPlaced().getLocation());
-        }
-    }
-
-    @EventHandler
-    public void onBlockPlaceNonCustom(BlockPlaceEvent event) {
-        if (!event.isCancelled()) {
-            ItemStack itemStack = event.getItemInHand();
-            if (itemStack.hasItemMeta()) {
-                ItemMeta itemMeta = itemStack.getItemMeta();
-                if (itemMeta.hasLore()) {
-                    if (itemMeta.getLore().size() > 0) {
-                        for (String line : itemMeta.getLore()) {
-                            String code = WolfyUtilities.unhideString(line);
-                            if (code.equals("cc_workbench")) {
-                                CustomCrafting.getWorkbenches().addWorkbench(event.getBlockPlaced().getLocation());
-                                CustomItems.setStoredBlockItem(event.getBlockPlaced().getLocation(), CustomItems.getCustomItem("customcrafting:workbench"));
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 
