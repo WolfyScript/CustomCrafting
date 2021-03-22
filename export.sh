702a9518df6a550b2fb0e9e64f8ea98a6055c0e3
WolfyScript <wolfyscript@gmail.com>
2021-03-22 21:57:43 +0100

    Changed shell script

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 source="D:\#Users\Gunnar\IntelliJ\CustomCrafting_2.0\target"
-target="D:\Minecraft\PluginTestServer\\"$1"\plugins\customcrafting.jar"
+target="D:\Minecraft\PluginTestServer\$1\plugins\customcrafting.jar"
 
 cd $source
 latest_file=$(ls -t | head -n 1)
