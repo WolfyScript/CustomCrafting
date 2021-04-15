4f94c284d2d3125cb65403315417768579b7aa2b
WolfyScript <wolfyscript@gmail.com>
2021-04-15 18:23:20 +0200

    Added Extension execution to GrindstoneRecipes

--- src/main/java/me/wolfyscript/customcrafting/recipes/types/grindstone/GrindstoneData.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/grindstone/GrindstoneData.java
@@ -1,16 +1,21 @@
 package me.wolfyscript.customcrafting.recipes.types.grindstone;
 
+import me.wolfyscript.customcrafting.utils.recipe_item.Result;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+
+import java.util.Optional;
 
 public class GrindstoneData {
 
     private final GrindstoneRecipe recipe;
+    private final Optional<Result<?>> result;
     private final CustomItem inputTop;
     private final CustomItem inputBottom;
     private final boolean validItem;
 
-    public GrindstoneData(GrindstoneRecipe recipe, boolean validItem, CustomItem inputTop, CustomItem inputBottom) {
+    public GrindstoneData(GrindstoneRecipe recipe, Optional<Result<?>> result, boolean validItem, CustomItem inputTop, CustomItem inputBottom) {
         this.recipe = recipe;
+        this.result = result;
         this.validItem = validItem;
         this.inputTop = inputTop;
         this.inputBottom = inputBottom;
@@ -26,6 +31,10 @@ public class GrindstoneData {
 
     public GrindstoneRecipe getRecipe() {
         return recipe;
+    }
+
+    public Optional<Result<?>> getResult() {
+        return result;
     }
 
     public boolean isValidItem() {
