73553f9fa914109aeba283d06735a3ee6dc8214b
WolfyScript <wolfyscript@gmail.com>
2021-04-19 19:51:28 +0200

    Added new Patreon

--- src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
+++ src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
@@ -18,23 +18,24 @@ public class Patreon {
 
     public void initialize() {
         addPatron(new Patron("Nat R", "956faa3f-df9e-402b-bc13-39c03d4b4a5b", Tier.ELITE));
+        addPatron(new Patron("Thomas Texier", "5780bf01-16df-4f58-a768-1a78445f3d87", Tier.ELITE));
         addPatron(new Patron("Apprehentice", "db61eab0-7fb1-48db-986f-125e73787976", Tier.WOLFRAM));
         addPatron(new Patron("Alex", "af1ef7e4-acc3-44a1-8323-8f50b92be2c9", Tier.WOLFRAM));
-        addPatron(new Patron("Thomas Texier", "5780bf01-16df-4f58-a768-1a78445f3d87", Tier.ELITE));
         addPatron(new Patron("Junye Zhou", Tier.DIAMOND));
         addPatron(new Patron("gizmonster", "e502d121-de9d-4f5d-b7e5-0da747c4e2e8", Tier.WOLFRAM));
-        addPatron(new Patron("John", Tier.WOLFRAM));
         addPatron(new Patron("HittmanA", Tier.WOLFRAM));
+        addPatron(new Patron("John", Tier.WOLFRAM));
         //addPatron(new Patron("Vincent Deniau", "a307c2b3-463a-4db6-8a6a-07419909af72", Tier.WOLFRAM));
         addPatron(new Patron("OTZ", Tier.WOLFRAM));
         addPatron(new Patron("Alexander", Tier.WOLFRAM));
+        addPatron(new Patron("The Hound Brothers", Tier.WOLFRAM));
         addPatron(new Patron("Jason Asada", Tier.WOLFRAM));
         //addPatron(new Patron("CypherPhyre", Tier.WOLFRAM));
-        addPatron(new Patron("The Hound Brothers", Tier.WOLFRAM));
         addPatron(new Patron("Xwdit", Tier.WOLFRAM));
         addPatron(new Patron("Fil", Tier.WOLFRAM));
         addPatron(new Patron("Pixelcraftian", Tier.WOLFRAM));
         addPatron(new Patron("Chaotic Chaos", Tier.WOLFRAM));
+        addPatron(new Patron("DTNTW", Tier.WOLFRAM));
         //addPatron(new Patron("Vincent Fournier"));
         //addPatron(new Patron("Inec Ackerr", Tier.WOLFRAM));
         //addPatron(new Patron("ValentineDesigns"));
