bd66857b94dcca50dbf41929e288496a29bb24d6
WolfyScript <wolfyscript@gmail.com>
2021-06-19 13:42:26 +0200

    Added ItemStack to IngredientData record.

--- src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/IngredientData.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/IngredientData.java
@@ -2,6 +2,7 @@ package me.wolfyscript.customcrafting.recipes.types.workbench;
 
 import me.wolfyscript.customcrafting.utils.recipe_item.Ingredient;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+import org.bukkit.inventory.ItemStack;
 
-public record IngredientData(Ingredient ingredient, CustomItem customItem) {
+public record IngredientData(Ingredient ingredient, CustomItem customItem, ItemStack itemStack) {
 }
