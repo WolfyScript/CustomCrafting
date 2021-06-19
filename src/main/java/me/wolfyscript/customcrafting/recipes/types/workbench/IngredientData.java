88bc0f961fdd5ea9d1b7b75dff42e3917b705d78
WolfyScript <wolfyscript@gmail.com>
2021-06-19 13:36:44 +0200

    Added IngredientData record to store data for each indexed grid slot.

---
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/IngredientData.java
@@ -0,0 +1,7 @@
+package me.wolfyscript.customcrafting.recipes.types.workbench;
+
+import me.wolfyscript.customcrafting.utils.recipe_item.Ingredient;
+import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+
+public record IngredientData(Ingredient ingredient, CustomItem customItem) {
+}
