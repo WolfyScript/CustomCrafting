8ba6dbdd80a30be2c0970b79505b94d0d9d0ecea
WolfyScript <wolfyscript@gmail.com>
2021-06-20 22:09:01 +0200

    ResultTarget is cloned correctly now and ignores the sub types on load (for now)

--- src/main/java/me/wolfyscript/customcrafting/utils/recipe_item/target/ResultTarget.java
+++ src/main/java/me/wolfyscript/customcrafting/utils/recipe_item/target/ResultTarget.java
@@ -3,14 +3,17 @@ package me.wolfyscript.customcrafting.utils.recipe_item.target;
 
 import me.wolfyscript.customcrafting.recipes.types.workbench.CraftingData;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+import me.wolfyscript.utilities.libraries.com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public abstract class ResultTarget {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ResultTarget {
 
     private final List<MergeOption> mergeOptions;
 
@@ -19,7 +22,7 @@ public abstract class ResultTarget {
     }
 
     protected ResultTarget(ResultTarget target) {
-        this.mergeOptions = target.mergeOptions; //TODO: Clone correctly!
+        this.mergeOptions = target.mergeOptions.stream().map(MergeOption::clone).collect(Collectors.toList());
     }
 
     /**
@@ -48,6 +51,17 @@ public abstract class ResultTarget {
      */
     public ItemStack mergeMisc(ItemStack[] ingredients, @Nullable Player player, ItemStack result) {
         return null;
+    }
+
+    /**
+     * Adds the specified {@link MergeOption} to the Target.
+     *
+     * @param option The MergeOption to add.
+     * @return The {@link ResultTarget} for chaining this method.
+     */
+    public ResultTarget addOption(MergeOption option) {
+        mergeOptions.add(option);
+        return this;
     }
 
 
