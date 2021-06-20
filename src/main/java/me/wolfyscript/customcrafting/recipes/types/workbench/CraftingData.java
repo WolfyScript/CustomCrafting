2502eb7530d5a55bd076a2c0aaf3ba2ecce36fcd
WolfyScript <wolfyscript@gmail.com>
2021-06-20 22:59:41 +0200

    Added more JavaDocs to CraftingData and IngredientData

--- src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/CraftingData.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/CraftingData.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
  * <p>
  * It indexes at which place of the grid which CustomItem is used, so it can use the CustomItem consume options, etc. a user might have saved.
  * <p>
- * The indexed Ingredients are used to target specific items and use it inside the Target options.
+ * The indexed Ingredients are used to target specific items and is used inside the {@link me.wolfyscript.customcrafting.utils.recipe_item.target.MergeOption}s.
  */
 public class CraftingData {
 
@@ -54,15 +54,40 @@ public class CraftingData {
         this.result = result;
     }
 
+    /**
+     * @return A Map of the Ingredients with the position inside the crafting grid.
+     */
     public Map<Vec2d, IngredientData> getIngredients() {
         return ingredients;
     }
 
+    /**
+     * The slots indicate the index (position) of the Ingredient inside the recipe.
+     * For normal recipes that means from 0 - 9.
+     * For Elite recipes the range is from 0 - 36.
+     * <p>
+     * For the correct position open the in-game Recipe Creator GUI and see in which slot the ingredient is.
+     * You may take the character saved in the config and use the index of it inside this String "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+     *
+     * @param slot The recipe slot to get the {@link IngredientData} for.
+     * @return The {@link IngredientData} of the specified recipe slot.
+     */
     @Nullable
     public IngredientData getBySlot(int slot) {
         return indexedBySlot.get(slot);
     }
 
+    /**
+     * The slots indicate the index (position) of the Ingredient inside the recipe.
+     * For normal recipes that means from 0 - 9.
+     * For Elite recipes the range is from 0 - 36.
+     * <p>
+     * For the correct position open the in-game Recipe Creator GUI and see in which slot the ingredient is.
+     * You may take the character saved in the config and use the index of it inside this String "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+     *
+     * @param slots The recipe slots to get the {@link IngredientData} for.
+     * @return A list of {@link IngredientData} of the specified recipe slots.
+     */
     public List<IngredientData> getBySlots(int[] slots) {
         List<IngredientData> list = new ArrayList<>();
         for (int slot : slots) {
--- src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/IngredientData.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/IngredientData.java
@@ -4,5 +4,14 @@ import me.wolfyscript.customcrafting.utils.recipe_item.Ingredient;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * Contains the data for a single Ingredient.
+ * <p>
+ * It contains:<br>
+ * - the recipe slot this data belongs to,<br>
+ * - the Ingredient of the recipe,<br>
+ * - the CustomItem that was chosen from that Ingredient,<br>
+ * - and the created ItemStack of the CustomItem.
+ */
 public record IngredientData(int recipeSlot, Ingredient ingredient, CustomItem customItem, ItemStack itemStack) {
 }
