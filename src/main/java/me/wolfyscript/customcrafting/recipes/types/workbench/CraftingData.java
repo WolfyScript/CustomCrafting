e681a30d78661e436e8efc8ef7045a20fb1a16e4
WolfyScript <wolfyscript@gmail.com>
2021-06-20 20:00:24 +0200

    CraftingData indexes the IngredientData to the recipe slot.

--- src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/CraftingData.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/types/workbench/CraftingData.java
@@ -4,7 +4,10 @@ import me.wolfyscript.customcrafting.recipes.types.CraftingRecipe;
 import me.wolfyscript.customcrafting.utils.geom.Vec2d;
 import me.wolfyscript.customcrafting.utils.recipe_item.Result;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,14 +21,16 @@ import java.util.stream.Collectors;
  */
 public class CraftingData {
 
-    CraftingRecipe<?> recipe;
-    Result<?> result;
-    Map<Vec2d, IngredientData> ingredients;
+    private final CraftingRecipe<?> recipe;
+    private final Map<Vec2d, IngredientData> ingredients;
+    private final Map<Integer, IngredientData> indexedBySlot;
+    private Result<?> result;
 
     public CraftingData(CraftingRecipe<?> recipe, Map<Vec2d, IngredientData> ingredients) {
         this.recipe = recipe;
         this.ingredients = ingredients;
         this.result = recipe.getResult();
+        this.indexedBySlot = ingredients.values().stream().collect(Collectors.toMap(IngredientData::recipeSlot, data -> data));
     }
 
     public CraftingRecipe<?> getRecipe() {
@@ -53,4 +58,16 @@ public class CraftingData {
         return ingredients;
     }
 
+    @Nullable
+    public IngredientData getBySlot(int slot) {
+        return indexedBySlot.get(slot);
+    }
+
+    public List<IngredientData> getBySlots(int[] slots) {
+        List<IngredientData> list = new ArrayList<>();
+        for (int slot : slots) {
+            list.add(getBySlot(slot));
+        }
+        return list;
+    }
 }
