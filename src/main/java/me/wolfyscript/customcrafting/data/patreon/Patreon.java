6b3bdba796ee3c59fcc7b2ee8df87df4667746f7
WolfyScript <wolfyscript@gmail.com>
2021-07-03 19:37:26 +0200

    Updated patreon Credit list

--- src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
+++ src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
@@ -38,6 +38,7 @@ public class Patreon {
         addPatron(new Patron("XiaoMoMi", Tier.WOLFRAM));
         addPatron(new Patron("yossy_zip", Tier.WOLFRAM));
         addPatron(new Patron("Jiří Hudský", Tier.WOLFRAM));
+        addPatron(new Patron("Mithran", Tier.WOLFRAM));
         addPatron(new Patron("Filip Oreško", Tier.SUPPORTER));
         //addPatron(new Patron("John", Tier.WOLFRAM));
         //addPatron(new Patron("Vincent Deniau", "a307c2b3-463a-4db6-8a6a-07419909af72", Tier.WOLFRAM));
