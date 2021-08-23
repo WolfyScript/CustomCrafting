e4e2d4e6a69caa0c9d749b26ada0876ebdc15885
WolfyScript <wolfyscript@gmail.com>
2021-08-23 19:56:48 +0200

    Changed default advanced crafting table recipe to the previous version.

--- src/main/java/me/wolfyscript/customcrafting/handlers/ConfigHandler.java
+++ src/main/java/me/wolfyscript/customcrafting/handlers/ConfigHandler.java
@@ -99,7 +99,7 @@ public class ConfigHandler {
 
             var workbenchCraft = new CraftingRecipeShaped(CustomCrafting.ADVANCED_CRAFTING_TABLE);
             workbenchCraft.setMirrorHorizontal(false);
-            workbenchCraft.setShape("G ", "C ", "DD");
+            workbenchCraft.setShape("G", "C", "D");
             workbenchCraft.setIngredient('G', new Ingredient(Material.GOLD_INGOT));
             workbenchCraft.setIngredient('C', new Ingredient(Material.CRAFTING_TABLE));
             workbenchCraft.setIngredient('D', new Ingredient(Material.GLOWSTONE_DUST));
