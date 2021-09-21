7086860e9b4c2851a072bbcb524e5a9c404cd97f
WolfyScript <wolfyscript@gmail.com>
2021-09-21 14:12:45 +0200

    Removed outdated and unused export.sh

--- export.sh
+++
@@ -1,7 +0,0 @@
-source="D:\#Users\Gunnar\IntelliJ\CustomCrafting_2.0\target"
-target="D:\Minecraft\PluginTestServer\$1\plugins\customcrafting.jar"
-
-cd $source
-latest_file=$(ls -t | head -n 1)
-cp -p "$latest_file" "$target"
-
