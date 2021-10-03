349caeba9708052917842f5d1071d2c42d76e826
WolfyScript <wolfyscript@gmail.com>
2021-10-03 17:13:54 +0200

    Updated Patreon credit.

--- src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
+++ src/main/java/me/wolfyscript/customcrafting/data/patreon/Patreon.java
@@ -15,8 +15,8 @@ public class Patreon {
     public void initialize() {
         addPatron(new Patron("Nat R", "956faa3f-df9e-402b-bc13-39c03d4b4a5b", Tier.ELITE));
         addPatron(new Patron("柏潭 吳", Tier.ELITE));
-        //addPatron(new Patron("Thomas Texier", "5780bf01-16df-4f58-a768-1a78445f3d87", Tier.ELITE));
         addPatron(new Patron("Junye Zhou", Tier.DIAMOND));
+        addPatron(new Patron("Jack Bruton", Tier.DIAMOND));
         addPatron(new Patron("Apprehentice", "db61eab0-7fb1-48db-986f-125e73787976", Tier.WOLFRAM));
         addPatron(new Patron("Alex", "af1ef7e4-acc3-44a1-8323-8f50b92be2c9", Tier.WOLFRAM));
         addPatron(new Patron("gizmonster", "e502d121-de9d-4f5d-b7e5-0da747c4e2e8", Tier.WOLFRAM));
@@ -24,7 +24,6 @@ public class Patreon {
         addPatron(new Patron("OTZ", Tier.WOLFRAM));
         addPatron(new Patron("The Hound Brothers", Tier.WOLFRAM));
         addPatron(new Patron("Xwdit", Tier.WOLFRAM));
-        //
         addPatron(new Patron("Pixelcraftian", Tier.WOLFRAM));
         addPatron(new Patron("DTNTW", Tier.WOLFRAM));
         addPatron(new Patron("Mr_Mint_", Tier.WOLFRAM));
@@ -36,47 +35,6 @@ public class Patreon {
         addPatron(new Patron("王聪聪", Tier.WOLFRAM));
         addPatron(new Patron("安田 安田", Tier.WOLFRAM));
         addPatron(new Patron("Vladyslav Kharchuk", Tier.WOLFRAM));
-        /*
-        addPatron(new Patron("Leo", Tier.WOLFRAM));
-        addPatron(new Patron("Matchman", Tier.WOLFRAM));
-
-        addPatron(new Patron("Venterok", Tier.WOLFRAM));
-        addPatron(new Patron("XiaoMoMi", Tier.WOLFRAM));
-        addPatron(new Patron("yossy_zip", Tier.WOLFRAM));
-        addPatron(new Patron("Jiří Hudský", Tier.WOLFRAM));
-        addPatron(new Patron("Qsycho", Tier.WOLFRAM));
-        addPatron(new Patron("GD4aming 4", Tier.WOLFRAM));
-        addPatron(new Patron("Advanced-Kind", Tier.WOLFRAM));
-        addPatron(new Patron("Piggy", Tier.WOLFRAM));
-        addPatron(new Patron("Filip Oreško", Tier.SUPPORTER));
-        //addPatron(new Patron("John", Tier.WOLFRAM));
-        //addPatron(new Patron("Vincent Deniau", "a307c2b3-463a-4db6-8a6a-07419909af72", Tier.WOLFRAM));
-
-        /*
-        addPatron(new Patron("HittmanA", Tier.WOLFRAM));
-        addPatron(new Patron("Fil", Tier.WOLFRAM));
-        addPatron(new Patron("carreb", Tier.WOLFRAM));
-        addPatron(new Patron("B01W", Tier.WOLFRAM));
-        addPatron(new Patron("Daniel Jagielczuk", Tier.WOLFRAM));
-        addPatron(new Patron("GustavoIs54", Tier.WOLFRAM));
-        addPatron(new Patron("Matěj Neumann", Tier.WOLFRAM));
-        addPatron(new Patron("NotChris Minecraft", Tier.WOLFRAM));
-        addPatron(new Patron("Xander Corcoran", Tier.WOLFRAM));
-        addPatron(new Patron("Jason Asada", Tier.WOLFRAM));
-
-        addPatron(new Patron("CypherPhyre", Tier.WOLFRAM));
-        addPatron(new Patron("Chaotic Chaos", Tier.WOLFRAM));
-        addPatron(new Patron("Vincent Fournier"));
-        addPatron(new Patron("Inec Ackerr", Tier.WOLFRAM));
-        addPatron(new Patron("ValentineDesigns"));
-        addPatron(new Patron("Gleaks mania"));
-        addPatron(new Patron("Arthur Neumann", Tier.WOLFRAM));
-        addPatron(new Patron("Ananass Me", "a599af6e-7f60-4050-9854-92026e29d4d1", Tier.WOLFRAM));
-        addPatron(new Patron("르 미"));
-        addPatron(new Patron("Beng701"));
-        addPatron(new Patron("Cameron R"));
-        addPatron(new Patron("Eli2t"));
-         */
     }
 
     private void addPatron(Patron patron) {
