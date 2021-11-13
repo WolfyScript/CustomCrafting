1ec4ad242f36d8bfeee66c4c724d7355073c0668
WolfyScript <wolfyscript@gmail.com>
2021-11-13 19:10:12 +0100

    Fixed icon for hide recipe button.

--- src/main/java/me/wolfyscript/customcrafting/gui/recipe_creator/ButtonHidden.java
+++ src/main/java/me/wolfyscript/customcrafting/gui/recipe_creator/ButtonHidden.java
@@ -30,12 +30,13 @@ import me.wolfyscript.utilities.util.inventory.PlayerHeadUtils;
 class ButtonHidden extends ToggleButton<CCCache> {
 
     ButtonHidden() {
-        super(ClusterRecipeCreator.HIDDEN.getKey(), (cache, guiHandler, player, guiInventory, i) -> cache.getRecipeCreatorCache().getRecipeCache().isHidden(), new ButtonState<>(ClusterRecipeCreator.HIDDEN_ENABLED, PlayerHeadUtils.getViaURL("ce9d49dd09ecee2a4996965514d6d301bf12870c688acb5999b6658e1dfdff85"), (cache, guiHandler, player, inventory, slot, event) -> {
-            cache.getRecipeCreatorCache().getRecipeCache().setHidden(false);
-            return true;
-        }), new ButtonState<>(ClusterRecipeCreator.HIDDEN_DISABLED, PlayerHeadUtils.getViaURL("85e5bf255d5d7e521474318050ad304ab95b01a4af0bae15e5cd9c1993abcc98"), (cache, guiHandler, player, inventory, slot, event) -> {
-            cache.getRecipeCreatorCache().getRecipeCache().setHidden(true);
-            return true;
-        }));
+        super(ClusterRecipeCreator.HIDDEN.getKey(), (cache, guiHandler, player, guiInventory, i) -> cache.getRecipeCreatorCache().getRecipeCache().isHidden(),
+                new ButtonState<>(ClusterRecipeCreator.HIDDEN_ENABLED, PlayerHeadUtils.getViaURL("85e5bf255d5d7e521474318050ad304ab95b01a4af0bae15e5cd9c1993abcc98"), (cache, guiHandler, player, inventory, slot, event) -> {
+                    cache.getRecipeCreatorCache().getRecipeCache().setHidden(false);
+                    return true;
+                }), new ButtonState<>(ClusterRecipeCreator.HIDDEN_DISABLED, PlayerHeadUtils.getViaURL("ce9d49dd09ecee2a4996965514d6d301bf12870c688acb5999b6658e1dfdff85"), (cache, guiHandler, player, inventory, slot, event) -> {
+                    cache.getRecipeCreatorCache().getRecipeCache().setHidden(true);
+                    return true;
+                }));
     }
 }
