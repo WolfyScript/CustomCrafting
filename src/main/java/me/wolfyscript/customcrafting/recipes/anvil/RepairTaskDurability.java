86f315509d354213f6649cb789ce7df5bfd9d46d
WolfyScript <wolfyscript@gmail.com>
2022-03-10 19:44:06 +0100

    Updated WU dependency to 3.16.3.2

--- src/main/java/me/wolfyscript/customcrafting/recipes/anvil/RepairTaskDurability.java
+++ src/main/java/me/wolfyscript/customcrafting/recipes/anvil/RepairTaskDurability.java
@@ -29,7 +29,6 @@ import me.wolfyscript.utilities.api.WolfyUtilCore;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
 import me.wolfyscript.utilities.compatibility.plugins.ItemsAdderIntegration;
 import me.wolfyscript.utilities.compatibility.plugins.itemsadder.CustomStack;
-import me.wolfyscript.utilities.compatibility.plugins.itemsadder.ItemsAdderRef;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
