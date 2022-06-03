47df55a7ead540742b8a16234df23d1510ddb188
WolfyScript <wolfyscript@gmail.com>
2022-06-03 14:07:43 +0200

    Slightly reduced iterations in CraftManager#getIngredients

--- src/main/java/me/wolfyscript/customcrafting/utils/CraftManager.java
+++ src/main/java/me/wolfyscript/customcrafting/utils/CraftManager.java
@@ -22,6 +22,7 @@
 
 package me.wolfyscript.customcrafting.utils;
 
+import com.google.common.collect.Lists;
 import me.wolfyscript.customcrafting.CustomCrafting;
 import me.wolfyscript.customcrafting.data.CCPlayerData;
 import me.wolfyscript.customcrafting.listeners.customevents.CustomPreCraftEvent;
@@ -220,10 +221,10 @@ public class CraftManager {
 
     public MatrixData getIngredients(ItemStack[] ingredients) {
         List<List<ItemStack>> items = new LinkedList<>();
-        List<ItemStack> ingredList = Arrays.asList(ingredients);
+        List<ItemStack> ingredList = Lists.newArrayList(ingredients);
         int gridSize = gridSize(ingredients);
         for (int y = 0; y < gridSize; y++) {
-            items.add(new ArrayList<>(ingredList.subList(y * gridSize, gridSize + y * gridSize)));
+            items.add(ingredList.subList(y * gridSize, gridSize + y * gridSize));
         }
         //Go through each row beginning from the top, removing empty rows, until you hit a non-empty row.
         int yPosOfFirstOccurrence = 0;
