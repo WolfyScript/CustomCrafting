7af56d4dc7656682164984fcaebb9f2202956965
WolfyScript <wolfyscript@gmail.com>
2022-07-06 13:36:48 +0200

    Fixed invalid furnace recipe xp calculation

--- src/main/java/me/wolfyscript/customcrafting/listeners/FurnaceListener.java
+++ src/main/java/me/wolfyscript/customcrafting/listeners/FurnaceListener.java
@@ -189,8 +189,7 @@ public class FurnaceListener implements Listener {
         while (expLevels > 0) {
             int totalXp = getExperienceValue(expLevels);
             expLevels -= totalXp;
-            int finalExp = expLevels;
-            loc.getWorld().spawn(loc, ExperienceOrb.class, orb -> orb.setExperience(finalExp));
+            loc.getWorld().spawn(loc, ExperienceOrb.class, orb -> orb.setExperience(totalXp));
         }
     }
 
