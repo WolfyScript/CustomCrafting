9766becbe5e9adb3281fb5e6853e7488dba0e58c
WolfyScript <wolfyscript@gmail.com>
2022-08-03 22:55:51 +0200

    Added CrossCompFunction to support 4.x and 5.x ProtocolLib

---
+++ src/main/java/me/wolfyscript/customcrafting/compatibility/protocollib/CrossCompFunction.java
@@ -0,0 +1,31 @@
+/*
+ *       ____ _  _ ____ ___ ____ _  _ ____ ____ ____ ____ ___ _ _  _ ____
+ *       |    |  | [__   |  |  | |\/| |    |__/ |__| |___  |  | |\ | | __
+ *       |___ |__| ___]  |  |__| |  | |___ |  \ |  | |     |  | | \| |__]
+ *
+ *       CustomCrafting Recipe creation and management tool for Minecraft
+ *                      Copyright (C) 2021  WolfyScript
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.wolfyscript.customcrafting.compatibility.protocollib;
+
+import java.util.function.UnaryOperator;
+
+public abstract class CrossCompFunction<T> implements UnaryOperator<T>, java.util.function.Function<T, T> {
+
+    @Override
+    public abstract T apply(T s);
+}
