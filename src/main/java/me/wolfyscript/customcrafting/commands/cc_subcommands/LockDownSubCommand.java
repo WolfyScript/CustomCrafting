602e8dc2e0d61c2b00058dd9273ca71159aa6c1d
WolfyScript <wolfyscript@gmail.com>
2024-01-10 18:06:46 +0100

    The lockdown command requires a confirmation now

--- src/main/java/me/wolfyscript/customcrafting/commands/cc_subcommands/LockDownSubCommand.java
+++ src/main/java/me/wolfyscript/customcrafting/commands/cc_subcommands/LockDownSubCommand.java
@@ -26,7 +26,7 @@ import me.wolfyscript.customcrafting.CustomCrafting;
 import me.wolfyscript.customcrafting.commands.AbstractSubCommand;
 import me.wolfyscript.customcrafting.utils.ChatUtils;
 import me.wolfyscript.lib.net.kyori.adventure.platform.bukkit.BukkitComponentSerializer;
-import me.wolfyscript.lib.net.kyori.adventure.text.Component;
+import me.wolfyscript.lib.net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import me.wolfyscript.utilities.api.WolfyUtilities;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -44,16 +44,32 @@ public class LockDownSubCommand extends AbstractSubCommand {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull String var3, @NotNull String[] var4) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull String var3, @NotNull String[] args) {
         WolfyUtilities api = customCrafting.getApi();
         if (ChatUtils.checkPerm(sender, "customcrafting.cmd.lockdown")) {
-            customCrafting.getConfigHandler().getConfig().toggleLockDown();
-            if (sender instanceof Player) {
-                if (customCrafting.getConfigHandler().getConfig().isLockedDown()) {
-                    api.getChat().sendMessage((Player) sender, api.getChat().translated("commands.lockdown.enabled"));
-                } else {
-                    api.getChat().sendMessage((Player) sender, api.getChat().translated("commands.lockdown.disabled"));
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("confirm")) {
+                    customCrafting.getConfigHandler().getConfig().toggleLockDown();
+
+                    if (sender instanceof Player) {
+                        if (customCrafting.getConfigHandler().getConfig().isLockedDown()) {
+                            api.getChat().sendMessage((Player) sender, api.getChat().translated("commands.lockdown.enabled"));
+                        } else {
+                            api.getChat().sendMessage((Player) sender, api.getChat().translated("commands.lockdown.disabled"));
+                        }
+                    } else if (sender instanceof ConsoleCommandSender consoleSender) {
+                        if (customCrafting.getConfigHandler().getConfig().isLockedDown()) {
+                            consoleSender.sendMessage(BukkitComponentSerializer.legacy().serialize(api.getChat().translated("commands.lockdown.enabled")));
+                        } else {
+                            consoleSender.sendMessage(BukkitComponentSerializer.legacy().serialize(api.getChat().translated("commands.lockdown.disabled")));
+                        }
+                    }
+                    return true;
                 }
+            }
+
+            if (sender instanceof Player) {
+                api.getChat().sendMessage((Player) sender, api.getChat().translated("commands.lockdown.confirm", Placeholder.parsed("state", customCrafting.getConfigHandler().getConfig().isLockedDown() ? "<green>ON</green>" : "<red>OFF</red>")));
             } else if (sender instanceof ConsoleCommandSender consoleSender) {
                 if (customCrafting.getConfigHandler().getConfig().isLockedDown()) {
                     consoleSender.sendMessage(BukkitComponentSerializer.legacy().serialize(api.getChat().translated("commands.lockdown.enabled")));
