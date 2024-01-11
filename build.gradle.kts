aa74d1c8c8fc78714e6b167f7522f33067f711e6
WolfyScript <wolfyscript@gmail.com>
2024-01-11 14:18:38 +0100

    Bump version to 4.16.9-beta.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 }
 
 group = "com.wolfyscript.customcrafting"
-version = "4.16.9-beta.6"
+version = "4.16.9-beta.7"
 description = "customcrafting-spigot"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
