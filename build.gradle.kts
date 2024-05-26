ac14885362dec20fb4ec44bca4cd715e6698de8d
WolfyScript <wolfyscript@gmail.com>
2024-05-26 15:45:47 +0200

    Update paper test server to 1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,27 +130,26 @@ minecraftServers {
         register("spigot_1_20_6") {
             version.set("1.20.6")
             type.set("SPIGOT")
-            imageVersion.set("java21-graalvm")
-
-            extraEnv.put("BUILD_FROM_SOURCE", "true")
+            imageVersion.set("java21")
             ports.set(setOf(debugPortMapping, "25569:25565"))
         }
         // Paper test servers
         register("paper_1_20") {
-            version.set("1.20.4")
+            version.set("1.20.6")
             type.set("PAPER")
-            ports.set(setOf(debugPortMapping, "25569:25565"))
+            imageVersion.set("java21")
+            ports.set(setOf("5007:5007", "25570:25565"))
         }
         register("paper_1_19") {
             version.set("1.19.4")
             type.set("PAPER")
-            ports.set(setOf(debugPortMapping, "25570:25565"))
+            ports.set(setOf(debugPortMapping, "25571:25565"))
         }
         // Purpur
         register("purpur_1_20") {
             version.set("1.20.4")
             type.set("PURPUR")
-            ports.set(setOf(debugPortMapping, "25569:25565"))
+            ports.set(setOf(debugPortMapping, "25572:25565"))
         }
     }
 }
