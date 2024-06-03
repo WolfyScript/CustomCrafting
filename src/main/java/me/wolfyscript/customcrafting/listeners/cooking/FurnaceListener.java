7a10067e140a44f5efc2c7a620e934bc8c040e8d
WolfyScript <wolfyscript@gmail.com>
2024-06-03 20:00:52 +0200

    Simplify Furnace logic & Add recipe caching

--- src/main/java/me/wolfyscript/customcrafting/listeners/cooking/FurnaceListener.java
+++ src/main/java/me/wolfyscript/customcrafting/listeners/cooking/FurnaceListener.java
@@ -22,38 +22,35 @@
 
 package me.wolfyscript.customcrafting.listeners.cooking;
 
-import java.util.Optional;
+import com.wolfyscript.utilities.bukkit.world.items.reference.StackReference;
 import me.wolfyscript.customcrafting.CustomCrafting;
-import me.wolfyscript.customcrafting.recipes.CustomRecipe;
-import me.wolfyscript.customcrafting.recipes.CustomRecipeCooking;
-import me.wolfyscript.customcrafting.recipes.data.CookingRecipeData;
+import me.wolfyscript.customcrafting.recipes.*;
+import me.wolfyscript.customcrafting.recipes.conditions.Conditions;
+import me.wolfyscript.customcrafting.recipes.data.*;
 import me.wolfyscript.customcrafting.utils.NamespacedKeyUtils;
-import me.wolfyscript.customcrafting.utils.cooking.CookingManager;
-import me.wolfyscript.customcrafting.utils.cooking.FurnaceListener1_17Adapter;
 import me.wolfyscript.utilities.api.WolfyUtilities;
 import me.wolfyscript.utilities.api.inventory.custom_items.CustomItem;
 import me.wolfyscript.utilities.registry.RegistryCustomItem;
-import me.wolfyscript.utilities.util.Pair;
 import me.wolfyscript.utilities.util.inventory.InventoryUtils;
 import me.wolfyscript.utilities.util.inventory.ItemUtils;
-import me.wolfyscript.utilities.util.version.MinecraftVersions;
-import me.wolfyscript.utilities.util.version.ServerVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
 import org.bukkit.block.Furnace;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.FurnaceBurnEvent;
-import org.bukkit.event.inventory.FurnaceSmeltEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.CookingRecipe;
 import org.bukkit.inventory.FurnaceInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public class FurnaceListener implements Listener {
 
@@ -69,11 +66,6 @@ public class FurnaceListener implements Listener {
         this.customCrafting = customCrafting;
         this.api = customCrafting.getApi();
         this.registryCustomItem = api.getRegistries().getCustomItems();
-        if (ServerVersion.isAfterOrEq(MinecraftVersions.v1_17)) {
-            Bukkit.getPluginManager().registerEvents(new FurnaceListener1_17Adapter(customCrafting, manager), customCrafting);
-        } else {
-            customCrafting.getLogger().warning("Looks like you are using 1.16. This will impact the Cooking Recipe Performance! Please update to 1.17 and/or use Paper!");
-        }
     }
 
     @EventHandler
@@ -97,37 +89,85 @@ public class FurnaceListener implements Listener {
     @EventHandler
     public void onBurn(FurnaceBurnEvent event) {
         ItemStack input = event.getFuel();
-        for (CustomItem customItem : registryCustomItem.values()) {
+        registryCustomItem.getByItemStack(input).ifPresent(customItem -> {
             var fuelSettings = customItem.getFuelSettings();
             if (fuelSettings.getBurnTime() > 0 && customItem.isSimilar(input) && fuelSettings.getAllowedBlocks().contains(event.getBlock().getType())) {
                 event.setCancelled(false);
                 event.setBurning(true);
                 event.setBurnTime(fuelSettings.getBurnTime());
-                break;
             }
+        });
+    }
+
+    @EventHandler
+    public void onStartSmelt(FurnaceStartSmeltEvent event) {
+        manager.clearCache(event.getBlock());
+
+        final CookingRecipe<?> bukkitRecipe = event.getRecipe();
+        final ItemStack source = event.getSource();
+        final Block block = event.getBlock();
+        // We want to notify the SmeltEvent if the original recipe was a vanilla recipe, and if so let it use that instead when no custom recipe matches.
+        final boolean customBackingRecipe = ICustomVanillaRecipe.isPlaceholderOrDisplayRecipe(bukkitRecipe.getKey());
+
+        // Are there any cached recipes, that were previously matched with that bukkit recipe present?
+        Collection<me.wolfyscript.utilities.util.NamespacedKey> cachedRecipes = manager.getAssociatedCustomRecipes(bukkitRecipe.getKey());
+        for (me.wolfyscript.utilities.util.NamespacedKey recipeKey : cachedRecipes) {
+            CustomRecipeCooking<?, ?> customRecipe = (CustomRecipeCooking<?, ?>) customCrafting.getRegistries().getRecipes().get(recipeKey);
+            if (customRecipe == null) {
+                continue;
+            }
+            var dataOptional = processRecipe(customRecipe, source, block);
+            if (dataOptional.isEmpty()) {
+                continue;
+            }
+            manager.cacheRecipeData(event.getBlock(), new CookingRecipeCache(dataOptional.get(), customBackingRecipe));
+            return; // Found valid cached custom recipe
         }
+
+        // No cached recipe or no valid custom recipe
+        customCrafting.getRegistries().getRecipes().get((RecipeType<? extends CustomRecipeCooking<?, ?>>) switch (event.getBlock().getType()) {
+                    case BLAST_FURNACE -> RecipeType.BLAST_FURNACE;
+                    case SMOKER -> RecipeType.SMOKER;
+                    default -> RecipeType.FURNACE;
+                }).stream()
+                .sorted()
+                .filter(recipe -> !recipe.isDisabled() && !cachedRecipes.contains(recipe.getNamespacedKey())) // Do not include disabled or already checked recipes
+                .map(recipe1 -> processRecipe(recipe1, source, block))
+                .filter(Optional::isPresent)
+                .findFirst()
+                .ifPresentOrElse(dataOptional -> {
+                    // Let's remember this recipe and check it first next time the same bukkit recipe is present
+                    manager.cacheCustomBukkitRecipeAssociation(bukkitRecipe.getKey(), dataOptional.get().getRecipe().getNamespacedKey());
+                    // Finally cache recipe data and notify the SmeltEvent
+                    manager.cacheRecipeData(block, new CookingRecipeCache(dataOptional.get(), customBackingRecipe));
+                }, () -> {
+                    // No custom recipe was found, but may still want to cancel the smelting when the original recipe is a custom recipe
+                    manager.cacheRecipeData(block, new CookingRecipeCache(null, customBackingRecipe));
+                });
     }
 
     @EventHandler
     public void onSmelt(FurnaceSmeltEvent event) {
         //Check for and handle the custom recipe.
-        Pair<CookingRecipeData<?>, Boolean> data = manager.getCustomRecipeCache(event);
-        if (data.getValue()) {
-            if (data.getKey() == null) {
-                event.setCancelled(true);
+        Optional<CookingRecipeCache> cacheOptional = manager.getCustomRecipeCache(event);
+        if (cacheOptional.isPresent()) {
+            CookingRecipeCache cache = cacheOptional.get();
+            CookingRecipeData<?> data = cache.data();
+            if (data != null) {
+                applyResult(event);
                 return;
             }
-            manager.getAdapter().applyResult(event);
-            return;
+            if (cache.cancelSmelting()) {
+                event.setCancelled(true); // FurnaceStartSmeltEvent told us to cancel if no custom recipe was found
+            }
         }
-        Bukkit.getScheduler().runTask(customCrafting, () -> manager.clearCache(event.getBlock()));
-        //Similar to the check in the FurnaceStartSmeltEvent.
-        //This is needed in 1.16 as the FurnaceStartSmeltEvent doesn't exist.
-        //Check if the CustomItem is allowed in Vanilla recipes
+
+        // Check if the CustomItem is allowed in Vanilla recipes
         CustomItem customItem = CustomItem.getByItemStack(event.getSource());
         if (customItem != null && customItem.isBlockVanillaRecipes()) {
             event.setCancelled(true); //Cancel the process if it is.
         }
+        Bukkit.getScheduler().runTask(customCrafting, () -> manager.clearCache(event.getBlock()));
     }
 
     /**
@@ -159,6 +199,75 @@ public class FurnaceListener implements Listener {
             });
         }
     }
+
+    private Optional<CookingRecipeData<?>> processRecipe(CustomRecipeCooking<?,?> cookingRecipe, ItemStack source, Block block) {
+        if (cookingRecipe.validType(block.getType())) {
+            Optional<StackReference> customSource = cookingRecipe.getSource().checkChoices(source, cookingRecipe.isCheckNBT());
+            if (customSource.isPresent()) {
+                if (cookingRecipe.checkConditions(Conditions.Data.of(block))) {
+                    var data = new IngredientData(0, 0, cookingRecipe.getSource(), customSource.get(), source);
+                    return Optional.ofNullable(switch (cookingRecipe.getRecipeType().getType()) {
+                        case FURNACE -> new FurnaceRecipeData((CustomRecipeFurnace) cookingRecipe, data);
+                        case SMOKER -> new SmokerRecipeData((CustomRecipeSmoking) cookingRecipe, data);
+                        case BLAST_FURNACE -> new BlastingRecipeData((CustomRecipeBlasting) cookingRecipe, data);
+                        default -> null;
+                    });
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Applies the result to the furnace and clears the cached data afterwards.<br>
+     * <p>
+     * If there is no actively cached data for the specified block it won't do anything!
+     *
+     * @param event The event to set result for.
+     */
+    public void applyResult(FurnaceSmeltEvent event) {
+        var block = event.getBlock();
+        manager.getCustomRecipeCache(block).ifPresent(cache -> {
+            FurnaceInventory inventory = ((Furnace) event.getBlock().getState()).getInventory();
+            ItemStack smelting = inventory.getSmelting();
+            if (ItemUtils.isAirOrNull(smelting)) return;
+
+            var data = cache.data();
+            Bukkit.getScheduler().runTaskLater(customCrafting, () -> manager.clearCache(block), 1); //Clearing the cached data after 1 tick (event should be done).
+            if (data == null) return;
+            var result = data.getResult();
+            var currentResultItem = inventory.getResult();
+
+            ItemStack itemResult = result.item(data, null, block);
+            //Need to set the result to air to bypass the vanilla result computation (See net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity#burn).
+            event.setResult(new ItemStack(Material.AIR));
+            if (currentResultItem != null) {
+                if (!itemResult.isSimilar(currentResultItem)) {
+                    event.setCancelled(true);
+                    return;
+                }
+                int nextAmount = currentResultItem.getAmount() + itemResult.getAmount();
+                if (nextAmount > currentResultItem.getMaxStackSize()) {
+                    event.setCancelled(true);
+                    return;
+                }
+                currentResultItem.setAmount(nextAmount);
+            } else {
+                inventory.setResult(itemResult);
+            }
+
+            data.bySlot(0).ifPresent(ingredientData -> {
+                ItemStack shrunken = ingredientData.reference().shrink(smelting, 1, ingredientData.ingredient().isReplaceWithRemains(), null, null, block.getLocation());
+                shrunken.setAmount(shrunken.getAmount());
+                inventory.setSmelting(shrunken);
+            });
+
+            result.executeExtensions(block.getLocation(), true, null);
+            result.removeCachedReference(block);
+            block.getState().update(); // Update the state of the block. Just in case!
+        });
+    }
+
 
     @Deprecated
     private void awardRecipeExperience(PersistentDataContainer usedRecipes, NamespacedKey bukkitRecipeKey, Location location) {
