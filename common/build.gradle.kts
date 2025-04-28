b755758115d4d792702f948ed494fd2af27d1cf0
WolfyScript <wolfyscript@gmail.com>
2025-04-28 23:33:26 +0200

    build: use fabric loom for common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,11 @@
 plugins {
     kotlin("jvm")
     id("build.settings.default")
+    alias(libs.plugins.fabric.loom)
+}
+
+dependencies {
+    api(project(":api"))
+    minecraft(libs.minecraft)
+    mappings(loom.officialMojangMappings())
 }
