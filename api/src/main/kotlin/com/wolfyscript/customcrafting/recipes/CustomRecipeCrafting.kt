03beb47efad95940a7aeaebd8bff2a2c27e6eddd
WolfyScript <wolfyscript@gmail.com>
2025-05-03 22:12:57 +0200

    api: add values and docs to CraftingFormula

--- api/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeCrafting.kt
+++ api/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeCrafting.kt
@@ -1,5 +1,10 @@
 package com.wolfyscript.customcrafting.recipes
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
 interface CustomRecipeCrafting : CustomRecipe<CustomRecipeCrafting> {
 
     val formula: CraftingFormula
@@ -8,14 +13,30 @@ interface CustomRecipeCrafting : CustomRecipe<CustomRecipeCrafting> {
 
 }
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CUSTOM, property = "type")
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+// This could in theory be expanded to allow custom types from a type registry, but for now there is no need for that
+@JsonSubTypes(
+    JsonSubTypes.Type(value = CraftingFormula.Shapeless::class, name = "shapeless"),
+    JsonSubTypes.Type(value = CraftingFormula.Shaped::class, name = "shaped")
+)
+@JsonPropertyOrder(value = ["type"])
 interface CraftingFormula {
 
     val ingredients: List<Ingredient>
 
+    /**
+     * A crafting formula with a list of ingredients that can be arranged in any order
+     */
     interface Shapeless : CraftingFormula {
 
     }
 
+    /**
+     * A crafting formula that requires ingredients to be arranged in a specified shape.
+     *
+     * The shape may allow ingredients to be arranged mirrored (see [ShapeSymmetry]).
+     */
     interface Shaped : CraftingFormula {
 
         val shape: Shape
@@ -23,10 +44,18 @@ interface CraftingFormula {
         val symmetry: ShapeSymmetry
 
         interface ShapeSymmetry {
-
+            val horizontal: Boolean
+            val vertical: Boolean
+            val rotate: Boolean
         }
 
         interface Shape {
+
+            /**
+             * A list of rows each with character keys that are associated with ingredients.
+             * The length of the list is fixed at 3, and so is the length of each row.
+             */
+            val rows: List<String>
 
         }
         
