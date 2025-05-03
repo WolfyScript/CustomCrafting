a505296757c94c49fe419c5df9e72440ca603cf1
WolfyScript <wolfyscript@gmail.com>
2025-05-03 16:34:16 +0200

    api: split ResourceSettings off into a separate file

--- api/src/main/kotlin/com/wolfyscript/customcrafting/configuration/resources/ResourceSettings.kt
+++ api/src/main/kotlin/com/wolfyscript/customcrafting/configuration/resources/ResourceSettings.kt
@@ -4,75 +4,18 @@ interface ResourceSettings {
 
     /**
      * A list of destinations to save resources to and load resources from.
+     *
+     * The list also specifies the order and therefore priority of destinations.
+     *
+     * The order in which the resources are loaded from destinations is equal to the order specified in this list.
+     * Destinations may overwrite resources loaded from other destinations that were loaded beforehand.
+     *
+     * The same applies to saving resources.
+     * So resources are first stored at the first destination and then to the next, etc.
+     * A destination can block resources stored to it to propagate to the next.
+     *
      */
     val destinations: List<DestinationSettings>
 
 }
 
-/**
- * Settings for a destination to save resources to and load resources from.
- */
-interface DestinationSettings {
-
-    /**
-     * Optional filter to specify which resources to save to this destination.
-     * If not specified, all resources will be saved to this destination.
-     */
-    val filter: FilterSettings?
-
-    /**
-     * Whether to overwrite existing resources by resources from this destination.
-     */
-    val overwriteExisting: Boolean
-
-    /**
-     * Optional setting to use this destination as a backup destination.
-     * Backups are done before updates and resource upgrades.
-     *
-     * **Backup destinations are not used to load resources!**
-     * **They are only used to save resources! Filter settings still apply!**
-     */
-    val backup: BackupSettings?
-
-    interface LocalDestinationSettings : DestinationSettings {
-
-        /**
-         * An optional path to the resource directory.
-         */
-        val path: String?
-
-    }
-
-    interface SQLDestinationSettings : DestinationSettings {
-
-        val host: String
-        val port: Int
-        val schema: String
-        val username: String
-        val password: String
-
-    }
-
-    interface FilterSettings {
-
-        /**
-         * Namespaces that are saved to this destination.
-         * Only resources with these namespaces will be saved to this destination.
-         */
-        val includeNamespaces: List<String>
-
-        /**
-         * Namespaces that are not saved to this destination.
-         * Resources with these namespaces will not be saved to this destination.
-         */
-        val excludeNamespaces: List<String>
-
-    }
-
-    interface BackupSettings {
-
-        // TODO
-
-    }
-
-}
