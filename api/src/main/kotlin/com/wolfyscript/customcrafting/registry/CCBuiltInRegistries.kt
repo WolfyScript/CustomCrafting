2b8948d36eb47915fc9346ac64bc5b2bce696140
WolfyScript <wolfyscript@gmail.com>
2025-05-08 22:15:43 +0200

    api: add CustomRecipe registry

--- api/src/main/kotlin/com/wolfyscript/customcrafting/registry/CCBuiltInRegistries.kt
+++ api/src/main/kotlin/com/wolfyscript/customcrafting/registry/CCBuiltInRegistries.kt
@@ -1,10 +1,14 @@
 package com.wolfyscript.customcrafting.registry
 
+import com.wolfyscript.customcrafting.recipes.CustomRecipe
 import com.wolfyscript.customcrafting.recipes.RecipeType
+import com.wolfyscript.scafall.registry.Registry
 import com.wolfyscript.scafall.registry.TypeRegistry
 
 interface CCBuiltInRegistries {
 
     val recipeTypes: TypeRegistry<RecipeType<*>>
+
+    val customRecipes: Registry<CustomRecipe<*>>
 
 }
