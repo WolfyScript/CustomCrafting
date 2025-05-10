2eecfe5597ae614630d1a9600ad73fd1c824f48d
WolfyScript <wolfyscript@gmail.com>
2025-05-10 16:51:15 +0200

    impl: instantiate RecipeManager and DataManager

--- common/src/main/kotlin/com/wolfyscript/customcrafting/CustomCraftingCommon.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/CustomCraftingCommon.kt
@@ -1,15 +1,15 @@
 package com.wolfyscript.customcrafting
 
 import com.wolfyscript.customcrafting.recipes.RecipeManager
+import com.wolfyscript.customcrafting.recipes.RecipeManagerCommon
 import com.wolfyscript.customcrafting.registry.CCBuiltInRegistries
 import com.wolfyscript.customcrafting.resource.DataManager
+import com.wolfyscript.customcrafting.resource.DataManagerCommon
 
 abstract class CustomCraftingCommon : CustomCrafting {
 
-    override val recipeManager: RecipeManager
-        get() = TODO("Not yet implemented")
-    override val dataManager: DataManager
-        get() = TODO("Not yet implemented")
+    override val recipeManager: RecipeManager = RecipeManagerCommon()
+    override val dataManager: DataManager = DataManagerCommon()
     override val registries: CCBuiltInRegistries
         get() = TODO("Not yet implemented")
 
