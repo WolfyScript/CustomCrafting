310fc78604fb9c434fa0231d4e1201062a86e06a
WolfyScript <wolfyscript@gmail.com>
2025-05-22 14:41:20 +0200

    impl(common): stonecutter recipe logic

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeStonecuttingImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeStonecuttingImpl.kt
@@ -1,12 +1,35 @@
 package com.wolfyscript.customcrafting.recipes
 
+import com.wolfyscript.customcrafting.recipes.data.IngredientDataImpl
+import com.wolfyscript.customcrafting.recipes.data.RecipeData
+import com.wolfyscript.customcrafting.recipes.data.RecipeDataImpl
+import com.wolfyscript.scafall.wrappers.world.items.ItemStack
+
 class CustomRecipeStonecuttingImpl(
     override val priority: Int,
-    override val conditions: RecipeConditions
+    override val conditions: RecipeConditions,
+    override val source: Ingredient,
+    override val result: RecipeResult
 ) : CustomRecipeStonecutting {
 
     override val type: RecipeType<CustomRecipeStonecutting>
         get() = TODO("Not yet implemented")
+
+    override fun evaluate(
+        context: EvaluationContext,
+        stack: ItemStack,
+    ): RecipeData<CustomRecipeStonecutting>? {
+        if (!conditions.areSatisfied(context)) {
+            return null
+        }
+
+        val matchResult = source.match(stack, true)
+        if (matchResult == null) {
+            return null
+        }
+
+        return RecipeDataImpl(this, result, arrayOf(IngredientDataImpl(0, 0, source, matchResult)))
+    }
 
 
 }
