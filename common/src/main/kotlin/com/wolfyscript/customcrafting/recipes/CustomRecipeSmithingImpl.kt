ced9a895d298365c142a8eb65c9dd4bbff1d5699
WolfyScript <wolfyscript@gmail.com>
2025-05-22 18:14:06 +0200

    impl(common): smithing recipe logic

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeSmithingImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeSmithingImpl.kt
@@ -1,6 +1,9 @@
 package com.wolfyscript.customcrafting.recipes
 
+import com.wolfyscript.customcrafting.recipes.data.IngredientData
+import com.wolfyscript.customcrafting.recipes.data.IngredientDataImpl
 import com.wolfyscript.customcrafting.recipes.data.RecipeData
+import com.wolfyscript.customcrafting.recipes.data.RecipeDataImpl
 import com.wolfyscript.scafall.identifier.Key
 import com.wolfyscript.scafall.wrappers.world.items.ItemStack
 
@@ -23,7 +26,38 @@ class CustomRecipeSmithingImpl(
         baseStack: ItemStack?,
         additionStack: ItemStack?,
     ): RecipeData<CustomRecipeSmithing>? {
-        TODO("Not yet implemented")
+        if (!conditions.areSatisfied(context)) {
+            return null
+        }
+
+        if (template == null && templateStack != null || template != null && templateStack == null) {
+            return null
+        }
+        val matchedTemplate = template?.let {
+            it.match(templateStack!!, true)?.let { templateMatch ->
+                IngredientDataImpl(0, 0, template, templateMatch)
+            } ?: return null
+        }
+
+        if (base == null && baseStack != null || base != null && baseStack == null) {
+            return null
+        }
+        val matchedBase = base?.let {
+            it.match(baseStack!!, true)?.let { baseMatch ->
+                IngredientDataImpl(1, 1, base, baseMatch)
+            } ?: return null
+        }
+
+        if (addition == null && additionStack != null || addition != null && additionStack == null) {
+            return null
+        }
+        val matchedAddition = addition?.let {
+            it.match(additionStack!!, true)?.let { additionMatch ->
+                IngredientDataImpl(2, 2, addition, additionMatch)
+            } ?: return null
+        }
+
+        return RecipeDataImpl(this, result, arrayOf(matchedTemplate, matchedBase, matchedAddition))
     }
 
     data class CopyOptionsImpl(override val preserveComponents: List<Key>) : CustomRecipeSmithing.CopyOptions
