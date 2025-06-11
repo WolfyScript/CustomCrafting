8c80b194d36668e66ec130c610043d551d774a01
WolfyScript <wolfyscript@gmail.com>
2025-06-11 18:57:57 +0200

    api: remove unnecessary type property

--- api/src/main/kotlin/com/wolfyscript/customcrafting/recipes/RecipeType.kt
+++ api/src/main/kotlin/com/wolfyscript/customcrafting/recipes/RecipeType.kt
@@ -2,8 +2,6 @@ package com.wolfyscript.customcrafting.recipes
 
 interface RecipeType<T: CustomRecipe<*,*>> {
 
-    val type: Class<T>
-
     val recipeClass: Class<T>
 
     fun isInstance(recipe: CustomRecipe<*,*>): Boolean
