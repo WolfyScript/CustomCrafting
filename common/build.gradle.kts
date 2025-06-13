9a875776f6f6bcecd0169115b454866b6163756b
WolfyScript <wolfyscript@gmail.com>
2025-06-13 18:46:43 +0200

    build: do not remap common to intermediate mappings

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,3 +10,15 @@ dependencies {
     compileOnly(libs.jackson.kotlin)
     mappings(loom.officialMojangMappings())
 }
+
+tasks {
+    // Disable remapping without having to disable the tasks
+    // This will get shaded into other platforms that then use their specific remapper instead.
+    // Additionally, this will be a public api, which should work across all platforms.
+    remapJar {
+        targetNamespace = "named"
+    }
+    remapSourcesJar {
+        targetNamespace = "named"
+    }
+}
