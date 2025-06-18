b2034c1d11596748abb7ee34a663e814d52aecbe
WolfyScript <wolfyscript@gmail.com>
2025-06-18 23:25:05 +0200

    fix: actually check for **exact** shape and matrix size

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeCraftingImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeCraftingImpl.kt
@@ -76,7 +76,7 @@ class ShapedCraftingFormulaImpl(
         ingredientShape: Array<Int>,
         recipeCrafting: CustomRecipeCrafting,
     ): RecipeData<CustomRecipeCrafting>? {
-        if (matrix.width < shape.width || matrix.height < shape.height) {
+        if (matrix.width != shape.width || matrix.height != shape.height) {
             return null
         }
         val ingredientData: Array<IngredientData?> = Array(matrix.width * shape.height) { null }
