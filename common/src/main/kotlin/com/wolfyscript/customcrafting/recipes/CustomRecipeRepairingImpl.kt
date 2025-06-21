d301e7838857857a987471c9c2a29fe3f10d0155
WolfyScript <wolfyscript@gmail.com>
2025-06-21 22:06:13 +0200

    impl: CustomRecipeRepairing evaluate logic

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeRepairingImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeRepairingImpl.kt
@@ -1,7 +1,9 @@
 package com.wolfyscript.customcrafting.recipes
 
 import com.wolfyscript.customcrafting.recipes.conditions.RecipeConditions
+import com.wolfyscript.customcrafting.recipes.data.IngredientDataImpl
 import com.wolfyscript.customcrafting.recipes.data.RecipeData
+import com.wolfyscript.customcrafting.recipes.data.RecipeDataImpl
 import com.wolfyscript.customcrafting.recipes.data.RecipeInput
 import com.wolfyscript.scafall.wrappers.world.items.ItemStack
 import kotlin.random.Random
@@ -10,21 +12,35 @@ class CustomRecipeRepairingImpl(
     override val priority: Int,
     override val conditions: RecipeConditions,
     override val process: CustomRecipeRepairing.RepairProcess,
+    override val base: Ingredient,
+    override val addition: Ingredient?,
 ) : CustomRecipeRepairing {
 
     override fun evaluate(
         input: RecipeInput.RepairingRecipeInput,
         context: EvaluationContext,
     ): RecipeData<CustomRecipeRepairing>? {
-        TODO("Not yet implemented")
+        val matchedBase = base.match(input.base)?.let { baseMatch ->
+            IngredientDataImpl(1, 1, base, baseMatch)
+        } ?: return null
+
+        if (addition == null && input.addition != null || addition != null && input.addition == null) {
+            return null
+        }
+        val matchedAddition = addition?.match(input.addition!!)?.let { additionMatch ->
+            IngredientDataImpl(2, 2, addition, additionMatch)
+        } ?: return null
+
+        return RecipeDataImpl(this, arrayOf(matchedBase, matchedAddition))
     }
 
-    class FixedResultImpl(override val result: RecipeResult, override val cost: Int?) : CustomRecipeRepairing.RepairProcess.FixedResult {
+    class FixedResultImpl(override val result: RecipeResult, override val cost: Int?) :
+        CustomRecipeRepairing.RepairProcess.FixedResult {
 
         override fun compute(
             recipeData: RecipeData<CustomRecipeRepairing>,
             context: EvaluationContext,
-            random: Random
+            random: Random,
         ): ItemStack {
             return result.compute(recipeData, context, random)
         }
@@ -36,7 +52,7 @@ class CustomRecipeRepairingImpl(
         override fun compute(
             recipeData: RecipeData<CustomRecipeRepairing>,
             context: EvaluationContext,
-            random: Random
+            random: Random,
         ): ItemStack {
             TODO("Not yet implemented")
         }
