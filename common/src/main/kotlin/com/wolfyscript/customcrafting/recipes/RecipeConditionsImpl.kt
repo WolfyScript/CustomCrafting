986b6f6a88bb3bf9b0603fc27c9f302f270076c4
WolfyScript <wolfyscript@gmail.com>
2025-06-21 16:17:25 +0200

    impl: add default conditions empty list

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/RecipeConditionsImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/RecipeConditionsImpl.kt
@@ -3,7 +3,7 @@ package com.wolfyscript.customcrafting.recipes
 import com.wolfyscript.customcrafting.recipes.conditions.Condition
 import com.wolfyscript.customcrafting.recipes.conditions.RecipeConditions
 
-class RecipeConditionsImpl(override val conditions: List<Condition>) : RecipeConditions {
+class RecipeConditionsImpl(override val conditions: List<Condition> = emptyList()) : RecipeConditions {
 
     override fun areSatisfied(evaluationContext: EvaluationContext): Boolean {
         return conditions.all { it.isSatisfied(evaluationContext) }
