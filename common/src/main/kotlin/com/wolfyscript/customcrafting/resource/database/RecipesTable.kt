af689739adde579613330f8b1e31a03d35755745
WolfyScript <wolfyscript@gmail.com>
2025-07-15 18:44:56 +0200

    impl(common): use dir and name as primary key in recipes table

--- common/src/main/kotlin/com/wolfyscript/customcrafting/resource/database/RecipesTable.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/resource/database/RecipesTable.kt
@@ -9,13 +9,14 @@ import org.jetbrains.exposed.v1.json.json
 /**
  * A table of recipes associated with a directory and name
  */
-object RecipesTable : Table() {
-    val id = integer("id").autoIncrement()
+object RecipesTable : Table("recipes") {
     val dir = varchar("dir", 255)
     val name = varchar("name", 255)
     val config = json(
         "config",
         { CustomCraftingProvider.get().dataManager.jacksonObjectMapper.writeValueAsString(it) },
         { CustomCraftingProvider.get().dataManager.jacksonObjectMapper.readValue<CustomRecipe<*,*>>(it) })
+
+    override val primaryKey = PrimaryKey(dir, name)
 
 }
