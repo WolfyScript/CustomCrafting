5337eb7df871b9105efbd80e011ddc7a697ac767
WolfyScript <wolfyscript@gmail.com>
2025-08-29 22:37:31 +0200

    impl(fabric): properly shrink stonecutter ingredient

--- fabric/src/main/java/com/wolfyscript/customcrafting/fabric/mixin/StonecutterResultSlotMixin.java
+++ fabric/src/main/java/com/wolfyscript/customcrafting/fabric/mixin/StonecutterResultSlotMixin.java
@@ -10,16 +10,22 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.StonecutterMenu;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net.minecraft.world.inventory.StonecutterMenu$2")
-public class StonecutterResultSlotMixin {
+public abstract class StonecutterResultSlotMixin extends Slot {
+
+    private StonecutterResultSlotMixin(Container container, int slot, int x, int y) {
+        super(container, slot, x, y);
+    }
 
     @Unique
     private StonecutterMenu stonecutterMenu;
@@ -29,24 +35,36 @@ public class StonecutterResultSlotMixin {
         this.stonecutterMenu = this$0;
     }
 
-    @Inject(
-        method = "onTake",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/inventory/ResultContainer;awardUsedRecipes(Lnet/minecraft/world/entity/player/Player;Ljava/util/List;)V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void onTakeCustom(Player player, ItemStack stack, CallbackInfo ci) {
+    @Redirect(method = "onTake", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;remove(I)Lnet/minecraft/world/item/ItemStack;"))
+    private ItemStack onCustomTake(Slot instance, int amount, Player player, ItemStack stack) {
         var resultContainer = ((StonecutterMenuAccessor) stonecutterMenu).getResultContainer();
         var resultInfo = ((CCResultContainerExt) resultContainer).getResultInfo();
         if (resultInfo == null || resultInfo.getRecipe().getValue() == null) {
-            return;
+            return instance.remove(amount);
         }
-        var level = player.level();
-        var context = new EvaluationContextImpl(MinecraftWrapperKt.wrap(player), MinecraftWrapperKt.wrap(player.position(), Key.fromMc(level.dimension().location())));
+
+        var playerLevel = player.level();
+        var context = new EvaluationContextImpl(MinecraftWrapperKt.wrap(player), MinecraftWrapperKt.wrap(player.position(), Key.fromMc(playerLevel.dimension().location())));
         RecipeResultStateKt.resetRecipeResult((ServerPlayer) player, resultInfo.getRecipe().getKey());
-        ((CustomRecipeStonecutting) resultInfo.getRecipe().getValue()).getResult().runActions(context, 1);
+
+        var recipe = resultInfo.getRecipe().getValue();
+        if (recipe instanceof CustomRecipeStonecutting customRecipe) {
+            customRecipe.getResult().runActions(context, 1);
+
+            var input = resultInfo.getData().bySlot(0);
+            if (input != null) {
+                var shrunken = MinecraftWrapperKt.unwrap(customRecipe.getSource().shrink(
+                    MinecraftWrapperKt.wrap(stonecutterMenu.getSlot(0).getItem()),
+                    1,
+                    input.getMatchedItemStackRef(),
+                    context,
+                    resultInfo
+                ));
+                instance.set(shrunken);
+                return shrunken;
+            }
+        }
+        return instance.remove(amount);
     }
 
 }
