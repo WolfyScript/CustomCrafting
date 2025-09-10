7a0d0e4ac4e778b9877c09a7fcaa65ddd34e5cb1
WolfyScript <wolfyscript@gmail.com>
2025-09-10 13:17:04 +0200

    fix: grindstone recipe matching

--- common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeGrindingImpl.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/recipes/CustomRecipeGrindingImpl.kt
@@ -24,21 +24,24 @@ class CustomRecipeGrindingImpl(
         var baseStack = input.base
         var additionStack = input.addition
 
-        if (baseStack == null && additionStack == null) {
+        val emptyBase = baseStack == null || baseStack.isEmpty
+        val emptyAddition = additionStack == null || additionStack.isEmpty
+
+        if (emptyBase && emptyAddition) {
             return null
         }
 
         var baseInvSlot = 0
         if (addition == null) {
             // This recipe has no addition, so the stack may be placed into the base or addition slot
-            if (baseStack == null) {
+            if (emptyBase) {
                 baseInvSlot = 1
                 baseStack = input.addition // So lets take the addition and use it as if it were placed in the base slot
                 additionStack = null
             }
         }
 
-        if (baseStack == null) {
+        if (baseStack == null || baseStack.isEmpty) {
             return null // the base slot should never be emtpy at this point
         }
 
@@ -46,7 +49,7 @@ class CustomRecipeGrindingImpl(
             IngredientDataImpl(baseInvSlot, 0, base, baseMatch)
         } ?: return null
 
-        if (addition == null && additionStack != null || addition != null && additionStack == null) {
+        if (addition == null && !emptyAddition || addition != null && emptyAddition) {
             return null
         }
         val matchedAddition = addition?.match(additionStack!!)?.let { additionMatch ->
