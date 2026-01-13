51c2c81cf4096152177ad6b07ab4169e3b634884
WolfyScript <wolfyscript@gmail.com>
2026-01-13 18:42:00 +0100

    refactor: no longer keep origin in ui state

--- common/src/main/kotlin/com/wolfyscript/customcrafting/editor/ui/recipe_editor/state/UIIngredientPreview.kt
+++ common/src/main/kotlin/com/wolfyscript/customcrafting/editor/ui/recipe_editor/state/UIIngredientPreview.kt
@@ -2,7 +2,6 @@ package com.wolfyscript.customcrafting.editor.ui.recipe_editor.state
 
 import com.wolfyscript.customcrafting.editor.domain.recipes.IngredientModel
 import com.wolfyscript.customcrafting.editor.domain.recipes.RecipeCraftingModel
-import com.wolfyscript.customcrafting.editor.ui.recipe_editor.state.toPreview
 import com.wolfyscript.scafall.identifier.Key
 import com.wolfyscript.scafall.wrappers.snapshot
 import com.wolfyscript.scafall.wrappers.world.items.ItemStackSnapshot
@@ -10,18 +9,12 @@ import net.minecraft.world.item.ItemStack
 
 interface UIIngredientPreview {
 
-    val origin: IngredientModel
-
     data class Custom(
-        @Deprecated("temporary. to be replaced with use-case system")
-        override val origin: IngredientModel.CustomIngredientModel,
         val icon: ItemStackSnapshot,
         val replaceWithRemains: Boolean,
     ) : UIIngredientPreview
 
     data class Saved(
-        @Deprecated("temporary. to be replaced with use-case system")
-        override val origin: IngredientModel.SavedIngredientModel,
         val key: Key,
     ) : UIIngredientPreview
 
@@ -30,13 +23,11 @@ interface UIIngredientPreview {
 fun IngredientModel.toPreview(): UIIngredientPreview? {
     return when (this) {
         is IngredientModel.CustomIngredientModel -> UIIngredientPreview.Custom(
-            this,
             stacks.firstOrNull()?.create()?.snapshot() ?: ItemStack.EMPTY.snapshot(),
             replaceWithRemains
         )
 
         is IngredientModel.SavedIngredientModel -> UIIngredientPreview.Saved(
-            this,
             key
         )
 
