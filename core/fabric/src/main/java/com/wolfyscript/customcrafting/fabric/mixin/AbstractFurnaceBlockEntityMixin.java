5462f109c14e973a8fe4f85d263216c1a02abad3
WolfyScript <wolfyscript@gmail.com>
2026-05-16 18:12:28 +0200

    impl(fabric): update furnace custom recipe burn mixin

--- core/fabric/src/main/java/com/wolfyscript/customcrafting/fabric/mixin/AbstractFurnaceBlockEntityMixin.java
+++ core/fabric/src/main/java/com/wolfyscript/customcrafting/fabric/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,31 +1,30 @@
 package com.wolfyscript.customcrafting.fabric.mixin;
 
-import com.wolfyscript.customcrafting.fabric.inject.RecipeInputSingleSlotCustomExt;
-import com.wolfyscript.customcrafting.fabric.inject.RecipeResultCacheExt;
-import com.wolfyscript.customcrafting.fabric.inject.RecipeResultStateCache;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.wolfyscript.customcrafting.core.recipes.CustomRecipeCooking;
 import com.wolfyscript.customcrafting.core.recipes.EvaluationContextImpl;
 import com.wolfyscript.customcrafting.core.recipes.data.RecipeInput;
 import com.wolfyscript.customcrafting.core.recipes.state.EvaluationContextState;
-import com.wolfyscript.scafall.wrappers.MinecraftWrapperKt;
+import com.wolfyscript.customcrafting.fabric.inject.RecipeInputSingleSlotCustomExt;
+import com.wolfyscript.customcrafting.fabric.inject.RecipeResultCacheExt;
+import com.wolfyscript.customcrafting.fabric.inject.RecipeResultStateCache;
+import com.wolfyscript.scafall.wrappers.minecraft.EntityWrappersKt;
+import com.wolfyscript.scafall.wrappers.minecraft.ItemStackWrappersKt;
+import com.wolfyscript.scafall.wrappers.minecraft.PositionWrappersKt;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.AbstractCookingRecipe;
-import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
@@ -39,80 +38,55 @@ public abstract class AbstractFurnaceBlockEntityMixin implements RecipeResultCac
         return resultStateCache;
     }
 
-    @Shadow
-    private static boolean burn(RegistryAccess par1, RecipeHolder<? extends AbstractCookingRecipe> par2, SingleRecipeInput par3, NonNullList<ItemStack> par4, int par5) {
-        return false;
-    }
-
-    @Shadow
-    protected abstract NonNullList<ItemStack> getItems();
-
-    @Shadow
-    protected static boolean canBurn(RegistryAccess registryAccess, @Nullable RecipeHolder<? extends AbstractCookingRecipe> recipe, SingleRecipeInput recipeInput, NonNullList<ItemStack> items, int maxStackSize) {
-        return false;
-    }
-
     @Inject(at = @At(value = "HEAD"), method = "serverTick")
-    private static void enterEvalContext(ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity furnace, CallbackInfo ci) {
-        EvaluationContextState.INSTANCE.enter(new EvaluationContextImpl(null, null, MinecraftWrapperKt.wrap(pos), MinecraftWrapperKt.wrap(furnace)));
+    private static void enterEvalContext(ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity entity, CallbackInfo ci) {
+        EvaluationContextState.INSTANCE.enter(new EvaluationContextImpl(null, null, PositionWrappersKt.wrap(pos), EntityWrappersKt.wrap(entity)));
     }
 
     @Inject(at = @At(value = "TAIL"), method = "serverTick")
-    private static void exitEvalContext(ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity furnace, CallbackInfo ci) {
+    private static void exitEvalContext(ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity entity, CallbackInfo ci) {
         EvaluationContextState.INSTANCE.exit();
     }
 
-    @ModifyVariable(method = "serverTick", at = @At(value = "STORE"), ordinal = 0)
-    private static SingleRecipeInput injectCustomDataIntoSingleRecipeInput(SingleRecipeInput singleRecipeInput, ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity furnace) {
-        var source = MinecraftWrapperKt.wrap(furnace.getItem(0));
-        ((RecipeInputSingleSlotCustomExt) (Object) singleRecipeInput).setCustomInput(RecipeInput.SingleSlotRecipeInput.Companion.of(source));
-        return singleRecipeInput;
+    @ModifyVariable(method = "serverTick", at = @At(value = "STORE"), name = "input")
+    private static SingleRecipeInput injectCustomDataIntoSingleRecipeInput(SingleRecipeInput input, ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity entity) {
+        var source = ItemStackWrappersKt.wrap(entity.getItem(0));
+        ((RecipeInputSingleSlotCustomExt) (Object) input).setCustomInput(RecipeInput.SingleSlotRecipeInput.Companion.of(source));
+        return input;
     }
 
-    @Redirect(
+    @WrapOperation(
+        method = "serverTick",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity;burn(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/item/crafting/RecipeHolder;Lnet/minecraft/world/item/crafting/SingleRecipeInput;Lnet/minecraft/core/NonNullList;I)Z"
-        ),
-        method = "serverTick"
+            target = "Lnet/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity;burn(Lnet/minecraft/core/NonNullList;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)V"
+        )
     )
-    private static boolean shrinkItemAndProduceResult(
-        RegistryAccess registryAccess,
-        RecipeHolder<? extends AbstractCookingRecipe> recipe,
-        SingleRecipeInput singleRecipeInput,
-        NonNullList<ItemStack> items,
-        int maxStackSize,
-        ServerLevel level, BlockPos blockPos, BlockState blockState, AbstractFurnaceBlockEntity entity
+    private static void wrapShrinkItemAndProduceResult(
+        NonNullList<ItemStack> items, ItemStack inputItemStack, ItemStack result, Operation<Void> original,
+        ServerLevel level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity entity,
+        @Local(name = "input") SingleRecipeInput input
     ) {
-        var resultInfo = ((RecipeInputSingleSlotCustomExt) (Object) singleRecipeInput).getResultInfo();
-        if (resultInfo == null || resultInfo.getRecipe().getValue() == null) {
+        var resultInfo = ((RecipeInputSingleSlotCustomExt) (Object) input).getResultInfo();
+        if (resultInfo == null || !(resultInfo.getRecipe().getValue() instanceof CustomRecipeCooking cookingRecipe)) {
             // Not a custom recipe, use vanilla logic
-            if (recipe != null && canBurn(registryAccess, recipe, singleRecipeInput, items, maxStackSize)) {
-                return burn(registryAccess, recipe, singleRecipeInput, items, maxStackSize);
-            }
-            return false;
+            original.call(items, inputItemStack, result);
+            return;
         }
 
-        var context = new EvaluationContextImpl(null, null, MinecraftWrapperKt.wrap(blockPos), MinecraftWrapperKt.wrap(entity));
-
-        var customRecipe = resultInfo.getRecipe().getValue();
-        if (!(customRecipe instanceof CustomRecipeCooking cookingRecipe)) {
-            return false;
-        }
+        var context = new EvaluationContextImpl(null, null, PositionWrappersKt.wrap(pos), EntityWrappersKt.wrap(entity));
         var random = ((RecipeResultCacheExt) entity).customcrafting$getRecipeResultStateCache()
             .get(resultInfo.getRecipe().getKey(), cookingRecipe.getResult().getAlwaysKeepPrevious()).getRandom();
-        var resultStack = MinecraftWrapperKt.unwrap(cookingRecipe.getResult().compute(resultInfo, context, random));
-        if (resultStack.isEmpty()) {
-            return false;
-        }
-
+        var resultStack = cookingRecipe.getResult().compute(resultInfo, context, random).unwrap();
         var existingResultStack = items.get(2);
+
         if (existingResultStack.isEmpty()) {
             items.set(2, resultStack);
         } else if (ItemStack.isSameItemSameComponents(existingResultStack, resultStack)) {
+            // TODO: Do we still need these checks? All of this should be checked by the canBurn method already
             if (existingResultStack.getCount() + resultStack.getCount() > existingResultStack.getMaxStackSize()) {
                 // Custom recipe but failed to produce result
-                return false;
+                return;
             }
             existingResultStack.grow(resultStack.getCount());
         }
@@ -123,16 +97,14 @@ public abstract class AbstractFurnaceBlockEntityMixin implements RecipeResultCac
         var sourceSlotData = resultInfo.getData().bySlot(0);
         if (sourceSlotData != null) {
             var newStack = cookingRecipe.getProcessing().getSource().shrink(
-                MinecraftWrapperKt.wrap(items.get(0)),
+                ItemStackWrappersKt.wrap(items.get(0)),
                 1,
                 sourceSlotData.getMatchedItemStackRef(),
                 context,
                 resultInfo
             );
-            items.set(0, MinecraftWrapperKt.unwrap(newStack));
+            items.set(0, newStack.unwrap());
         }
-
-        return true;
     }
 
 }
