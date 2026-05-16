75dd43635d0e83edc5503c1ded85c5592d2f38b0
WolfyScript <wolfyscript@gmail.com>
2026-05-16 21:57:19 +0200

    build: remove aio-fabric & add build to fabric-core

--- core/fabric/build.gradle.kts
+++ core/fabric/build.gradle.kts
@@ -1,6 +1,9 @@
+import utils.archiveName
+
 plugins {
     kotlin("jvm")
     `java-library`
+    alias(sharedLibs.plugins.shadow)
     id(sharedLibs.plugins.devtools.docker.minecraft.get().pluginId)
     id("build.settings.default")
     id("build.settings.fabric-loom")
@@ -18,8 +21,8 @@ loom {
 }
 
 dependencies {
-    api(projects.core.coreApi)
-    api(projects.core.coreCommon)
+    api(shadow(projects.core.coreApi)!!)
+    api(shadow(projects.core.coreCommon)!!)
 
     implementation(sharedLibs.bundles.exposed)
     implementation(sharedLibs.bundles.database.drivers)
@@ -27,4 +30,50 @@ dependencies {
 
     implementation(sharedLibs.fabric.loader)
     implementation(sharedLibs.fabric.api)
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("fabric.mod.json") {
+            expand("version" to project.version)
+        }
+    }
+    shadowJar {
+        // Mappings are in the runtime classpath. Not sure why they are included even though we use include for dependencies...
+        // So to be sure nothing else slips in, just accept dependencies from the shadow configuration.
+        configurations = listOf(project.configurations.shadow.get())
+        finalizedBy("fabric_copy")
+
+        dependencies {
+            include(project(project.projects.core.coreApi))
+            include(project(project.projects.core.coreCommon))
+
+            sharedLibs.bundles.sentry.get().forEach {
+                include(dependency(it))
+            }
+        }
+        metaInf.duplicatesStrategy = DuplicatesStrategy.FAIL
+        relocate("io.sentry", "com.wolfyscript.customcrafting.sentry")
+    }
+    java {
+//        withSourcesJar()
+    }
+}
+
+minecraftServers {
+    libName.set("${archiveName("fabric", sharedLibs.versions.minecraft.get())}.jar")
+    servers {
+        register("fabric") {
+            destPath.set("mods")
+            destFileName.set("customcrafting.jar")
+            version.set(sharedLibs.versions.minecraft.get())
+            type.set("FABRIC")
+            imageVersion.set("java${sharedLibs.versions.jdk.get()}")
+            ports.add("25569:25565")
+            extraEnv.put("MODRINTH_PROJECTS", "fabric-api, fabric-language-kotlin")
+            extraEnv.put("FABRIC_LOADER_VERSION", sharedLibs.versions.fabric.loader.get())
+        }
+    }
 }
--- core/fabric/src/main/kotlin/com/wolfyscript/customcrafting/fabric/CustomCraftingFabricMod.kt
+++ core/fabric/src/main/kotlin/com/wolfyscript/customcrafting/fabric/CustomCraftingFabricMod.kt
@@ -2,9 +2,9 @@ package com.wolfyscript.customcrafting.fabric
 
 import com.wolfyscript.customcrafting.CustomCraftingBoostrap
 import com.wolfyscript.customcrafting.core.commands.CCCommands
-import com.wolfyscript.customcrafting.fabric.api.CustomCraftingFabric
 import com.wolfyscript.customcrafting.core.sentry.initSentry
 import com.wolfyscript.customcrafting.core.util.CUSTOMCRAFTING_NAMESPACE
+import com.wolfyscript.customcrafting.fabric.api.CustomCraftingFabric
 import com.wolfyscript.scafall.ScafallProvider
 import com.wolfyscript.scafall.loader.ScafallLoader
 import io.sentry.Sentry
