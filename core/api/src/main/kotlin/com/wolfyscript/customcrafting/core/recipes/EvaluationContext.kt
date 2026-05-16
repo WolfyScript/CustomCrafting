55c4b84119b8cd90a005ecc143d78f1e429315e4
WolfyScript <wolfyscript@gmail.com>
2026-05-16 18:07:13 +0200

    impl: update new scafall wrappers

--- core/api/src/main/kotlin/com/wolfyscript/customcrafting/core/recipes/EvaluationContext.kt
+++ core/api/src/main/kotlin/com/wolfyscript/customcrafting/core/recipes/EvaluationContext.kt
@@ -1,9 +1,9 @@
 package com.wolfyscript.customcrafting.core.recipes
 
-import com.wolfyscript.scafall.wrappers.ScafallBlockEntity
-import com.wolfyscript.scafall.wrappers.ScafallPlayer
 import com.wolfyscript.scafall.wrappers.world.ScafallBlockPos
 import com.wolfyscript.scafall.wrappers.world.ScafallGlobalPrecisePos
+import com.wolfyscript.scafall.wrappers.world.entity.ScafallPlayer
+import com.wolfyscript.scafall.wrappers.world.level.block.entity.ScafallBlockEntity
 
 interface EvaluationContext {
 
